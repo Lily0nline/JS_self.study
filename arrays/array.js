437e558592e4c999b43e538341a3f81bc98bc004
YooJin Kim <lily@YooJins-Laptop.local>
2023-10-09 19:21:49 +0900

    new changes made

---
+++ arrays/array.js
@@ -0,0 +1,7 @@
+/*
+You can put any value inside an array. 
+Here, the array named arr even contains another array. 
+You can also insert values from other variables into the array.
+Insert the value of the variable string into the arr.
+Insert it in the right place so that the console.log() statement logs true.
+*/
