6baf2e61884b24c9180f15f79244151cf7561f42
YooJin Kim <lily@YooJins-Laptop.local>
2023-10-10 15:18:59 +0900

    arrow function practice

---
+++ arrow_function/arrow.js
@@ -0,0 +1,17 @@
+//transform this into an arrow function
+
+
+// const func = function (a) {
+//     const multiplier = 2;
+//     return a * multiplier;
+//   }
+    
+//   const result = func(10);
+//   console.log(result);
+
+
+const func = a => a * 2
+    
+  const result = func(10);
+  console.log(result);
+
