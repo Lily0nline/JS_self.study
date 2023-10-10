b9da9ed171ef8f7021a3ac48913bebbb3fd13059
YooJin Kim <lily@YooJins-Laptop.local>
2023-10-11 03:11:18 +0900

    synchronous programming

---
+++ callback/sync.js
@@ -0,0 +1,15 @@
+//adjust the code snippet so that the console.log statement logs the value 2
+
+// let i = 0 
+// function func(){
+//     i = 2;
+// }
+// setTimeout(func, 100); //change this line 
+// console.log(i)
+
+let i = 0 
+function func(){
+    i = 2;
+}
+func();
+console.log(i)
